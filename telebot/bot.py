10bb21216298371ec84abf50067738a0928a1954
Bharat Sharma <bs140598@gmail.com>
2020-08-29 13:33:25 +0530

    version v2

---
+++ telebot/bot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 29 13:19:50 2020
+
+@author: bharat
+"""
+def get_reply(text):
+    if text == "/help":
+        msg = "Under Construction"
+        return msg
+    
+    elif text == "/start":
+        msg = "At your service for some RED PILL content"
+        return msg
+    elif text == "/introduceme":
+        msg = "https://bookofpook.com/ \n https://therationalmale.com/ \n https://www.youtube.com/channel/UC8eLDzfH6YfZRDbDuMsNWiA \n https://www.youtube.com/user/EntrepreneursInCars"
+        return msg
